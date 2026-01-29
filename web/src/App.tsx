f1833e9ad4242c448034fb85824408119bb687a5
mccarthy-michael <macmike0604@gmail.com>
2026-01-29 16:22:46 +0000

    Add routing to simulate routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import { UserProvider, useUser } from "./lib/context/user";
+import { Routes, Route, Navigate } from "react-router";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import "./Login.css";
@@ -11,7 +12,22 @@ function AppContent() {
     return <div className="loading-container">Loading...</div>;
   }
 
-  return <>{current ? <Home /> : <Login />}</>;
+  return (
+    <Routes>
+      {!current ? (
+        <>
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </>
+      ) : (
+        <>
+          <Route path="/login" element={<Navigate to="/dashboard" />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="*" element={<Navigate to="/dashboard" />} />
+        </>
+      )}
+    </Routes>
+  );
 }
 
 export default function App() {
